a5b081e7af4a4250a66248a8ca086501778d6693
synergistics <kingjak678@gmail.com>
2015-08-13 00:19:00 -0400

    Largeish update: 	- Fixes error of non-terminating command calls after an error 		Before: An error could occur while linking files, yet the unfinished link would still be documented in the links.json file -- same with removing links 		After: If an error occurs in an asynchronous call, the function would terminate as expected 	- Adds documentation to commands for clarity

--- bin/autolink.js
+++ bin/autolink.js
@@ -1,5 +1,8 @@
+#!/usr/bin/env node
+
 var fs = require("fs");
 var path = require("path");
+
 
 var shellArgs = process.argv.slice(2);
 var linkCommand = shellArgs[0];
@@ -12,8 +15,19 @@ var commands = {
 	sync: sync
 }; 
 
-commands[linkCommand].apply(null, linkArgs);
+// Run autolink with command arguments
+(function () {
+	if (!commands.hasOwnProperty(linkCommand)){
+		console.log("Error: unknown command");	
+		return;
+	}	
 
+	commands[linkCommand].apply(null, linkArgs);
+})();
+
+// Initialize an linkFile in current directory
+// A linkFile keeps track of the links made by a user
+// The structure of a linkFile: {sourcePathAbsolute: [linkPathAbsolute]}
 function init(){
 	var file = fs.createWriteStream("./links.json", {flags: "wx"});
 
@@ -26,36 +40,41 @@ function init(){
 	});
 }
 
+// Create a symbolic link
 function link(sourcePath, linkPath){
 	fs.symlink(sourcePath, linkPath, function(err){
-		if(err){
-			console.log("Error: this link seems to already exist: " + linkPath);	
-		}	
-	})
-
-	var linkFile = path.dirname(sourcePath) + "/links.json";
-	
-	fs.readFile(linkFile, {encoding: "utf8"}, function(err, data){
-		var links = JSON.parse(data);
-		var sourcePathAbsolute = path.resolve(sourcePath);
-		var linkPathAbsolute = path.resolve(linkPath)
-
-		if (!(sourcePathAbsolute in links) || links[sourcePathAbsolute].constructor !== Array){
-			links[sourcePathAbsolute] = []; 
+		if (err){
+			console.log(err);
+			return;
 		}
+	
+		var linkFile = path.dirname(sourcePath) + "/links.json";
+		
+		fs.readFile(linkFile, {encoding: "utf8"}, function(err, data){
+			var links = JSON.parse(data);
+			var sourcePathAbsolute = path.resolve(sourcePath);
+			var linkPathAbsolute = path.resolve(linkPath)
 
-	  if (links[sourcePathAbsolute].indexOf(linkPathAbsolute) === -1){
-			links[sourcePathAbsolute].push(linkPathAbsolute);					 
-	  }	
+			// Make a new entry for a sourcePathAbsolute in linkFile if one does not exist
+			if (!(links.hasOwnProperty(sourcePathAbsolute)) || links[sourcePathAbsolute].constructor !== Array){
+				links[sourcePathAbsolute] = []; 
+			}
 
-		fs.writeFile(linkFile, JSON.stringify(links), function(err){
-			if (err){
-				console.log(err);	
+			// Add linkPathAbsolute to sourcePathAbsolute array in linkFile if one does not exist
+			if (links[sourcePathAbsolute].indexOf(linkPathAbsolute) === -1){
+				links[sourcePathAbsolute].push(linkPathAbsolute);					 
 			}	
+
+			fs.writeFile(linkFile, JSON.stringify(links), function(err){
+				if (err){
+					console.log(err);	
+				}	
+			});
 		});
 	});
 }
 
+// Remove a symbolic link if one exists in the linkFile in the sourcePath's directory
 function rmlink(sourcePath, linkPath){
 	var linkFile = path.dirname(sourcePath) + "/links.json";
 
@@ -63,7 +82,7 @@ function rmlink(sourcePath, linkPath){
 		var links = JSON.parse(data);
 		var sourcePathAbsolute = path.resolve(sourcePath);
 
-		if (linkPath != null){
+		if (typeof linkPath !== "undefined"){
 			var linkPathAbsolute = path.resolve(linkPath);		
 		}
 
@@ -76,34 +95,57 @@ function rmlink(sourcePath, linkPath){
 		});
 	});
 
+	// Handles removal of link in filesystem and in linkFile
 	function rm(obj, key, prop){
-		if (prop != null){	
+		// Remove specific symbolic link from a sourcePathAbsolute entry
+		if (typeof prop !== "undefined"){	
 			fs.unlink(prop, function(err){
 				if (err){
 					console.log(err);	
+					return;
 				}
-			});
-
-			obj[key] = obj[key].filter(function(element){
-				return element != prop;	
+			
+				obj[key] = obj[key].filter(function(element){
+					return element != prop;	
+				});
 			});
 		}
+		// Remove all symbolic links from a sourcePathAbsolute entry along with the entry itself
 		else {
+			var errors = [];
+
 			obj[key].forEach(function(element){
 				fs.unlink(element, function(err){
 					if (err){
 						console.log(err);	
+						errors.push(err);
 					}
 				})	
 			});	
-	
-			delete obj[key];
+			
+			if (errors.length === 0){
+				delete obj[key];
+			}
+			else {
+				console.log("Errors: Did not remove links");
+			}
 		}
 	}
 }
 
+// Synchronize a filesystem with the symbolic links specified at linkFilePath -- a links.json file
 function sync(linkFilePath){
+	if (typeof linkFilePath === "undefined"){
+		console.log("Error: No link file path provided");
+		return;
+	}
+
 	fs.readFile(linkFilePath, {encoding: "utf8"}, function(err, data){
+		if (err){
+			console.log(err);
+			return;
+		}
+
 		var links = JSON.parse(data);
 
 		for (sourcePath in links){
